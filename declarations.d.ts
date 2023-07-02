b60676fcd0717c0592784125bbb8c86a2157ff32
Max Patiiuk <max@patii.uk>
2023-07-02 14:08:43 -0700

    feat: Initial commit

---
+++ declarations.d.ts
@@ -0,0 +1,71 @@
+/**
+ * Fixes for various issues with default TypeScript declaration fils
+ */
+
+import type { IR, RA, RR, ValueOf } from './src/utils/types';
+
+/**
+ * Typescript does not recognize the definition overwrite when using
+ * the other method signature style
+ *
+ * Here and in the following, a Shorthand method signature is used
+ * to overwrite the default definitions
+ */
+
+declare global {
+  /**
+   * Workaround for https://github.com/microsoft/TypeScript/issues/17002
+   * Fix for Array.isArray() narrowing RA<T> to any[]
+   */
+  interface ArrayConstructor {
+    isArray(argument: RA<any> | any): argument is RA<any>;
+  }
+
+  interface Array {
+    /**
+     * A fix for Array.from(someValue).fill(otherValue) getting its type
+     * from someValue rather than otherValue
+     */
+    fill<V>(value: V): RA<V>;
+
+    // This won't be needed in TypeScript 5.0. See https://github.com/microsoft/TypeScript/issues/48829
+    findLastIndex(
+      predicate: (value: T, index: number, array: T[]) => unknown
+    ): number;
+  }
+
+  interface ReadonlyArray<T> {
+    findLastIndex(
+      predicate: (value: T, index: number, array: T[]) => unknown
+    ): number;
+  }
+
+  /**
+   * Prevent Object.entries() and Object.fromEntries() from widening key
+   * type to string
+   */
+  interface ObjectConstructor {
+    // Object
+    entries<DICTIONARY extends IR<unknown>>(
+      object: DICTIONARY
+    ): [
+      keyof DICTIONARY extends number ? string : string & keyof DICTIONARY,
+      ValueOf<DICTIONARY>
+    ][];
+
+    // Array
+    entries<ITEM>(array: RA<ITEM>): [string, ITEM][];
+
+    // Prevent Object.fromEntries() from widening the key type to string
+    fromEntries<KEYS extends PropertyKey, VALUES>(
+      entries: Iterable<readonly [KEYS, VALUES]>
+    ): RR<KEYS, VALUES>;
+
+    // Prevent Object.keys() from widening the key type to string[]
+    keys<KEY extends string>(object: RR<KEY, unknown>): RA<KEY>;
+  }
+
+  interface PromiseConstructor {
+    all<T>(values: Iterable<PromiseLike<T> | T>): Promise<RA<Awaited<T>>>;
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/*
+ * This config partially overwrites and extends the default Tailwind config:
+ * https://github.com/tailwindlabs/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  corePlugins: {
+    // Disable unneeded components to reduce performance impact
+    float: false,
+    clear: false,
+    skew: false,
+    caretColor: false,
+    sepia: false,
+    // Don't want to bother with adding back styles:
+    preflight: false,
+  },
+  darkMode: 'media',
+  plugins: [require('@tailwindcss/forms')],
+};
